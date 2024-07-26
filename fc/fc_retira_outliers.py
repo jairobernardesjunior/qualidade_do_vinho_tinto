e12591612bff7023730dee4c9217f01d34563cd7
jairobernardesjunior <jairobernardesjunior@gmail.com>
2024-07-26 16:15:59 -0300

    v 1.0
    implementando código

--- fc/fc_retira_outliers.py
+++ fc/fc_retira_outliers.py
@@ -2,25 +2,19 @@ import numpy as np
 
 def retira_outliers(dfx):
     column_names = list(dfx)
-    conta_out = 1
 
-    while conta_out > 0:
-        conta_out = 0
+    for names in column_names[1:]:
+        for x in [names]:
 
-        for names in column_names[1:]:
-            for x in [names]:
+            q75,q25 = np.percentile(dfx.loc[:,x],[75,25])
+            intr_qr = q75-q25
 
-                q75,q25 = np.percentile(dfx.loc[:,x],[75,25])
-                intr_qr = q75-q25
+            max = q75+(1.5*intr_qr)
+            min = q25-(1.5*intr_qr)
 
-                max = q75+(1.5*intr_qr)
-                min = q25-(1.5*intr_qr)
+            dfx.loc[dfx[x] < min,x] = np.nan
+            dfx.loc[dfx[x] > max,x] = np.nan
 
-                dfx.loc[dfx[x] < min,x] = np.nan
-                dfx.loc[dfx[x] > max,x] = np.nan
-
-                conta_out += dfx.loc[dfx[x] < min,x].count() + dfx.loc[dfx[x] > max,x].count()
-
-                dfx = dfx.dropna(axis = 0)
+    dfx = dfx.dropna(axis = 0)
             
     return dfx
